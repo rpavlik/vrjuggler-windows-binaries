7926ff3693a9f1b3ad8bf609e22a7fe61a8deebc
Ryan Pavlik <ryan.pavlik@snc.edu>
2011-10-25 11:30:17 -0500

    Extend, don't replace, the path.

--- vc9-cmd-prompt-here.cmd
+++ vc9-cmd-prompt-here.cmd
@@ -4,8 +4,8 @@ set VJ_BASE_DIR=%~dp0
 rem Set VJ_DEPS_DIR to be the same as VJ_BASE_DIR
 set VJ_DEPS_DIR=%VJ_BASE_DIR%
 
-rem Set the PATH to include both the bin and lib directories, so DLLs may be found.
-set PATH=%VJ_BASE_DIR%bin;%VJ_BASE_DIR%lib
+rem Extend the PATH to include both the bin and lib directories, so DLLs may be found.
+set PATH=%VJ_BASE_DIR%bin;%VJ_BASE_DIR%lib;%PATH%
 
 rem Run a command prompt that includes the Visual Studio 2008 settings.
 %comspec% /k ""%VS90COMNTOOLS%vsvars32.bat""
