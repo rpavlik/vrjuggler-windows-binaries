157935412f1449b3b5ae8fa872dc78bcc4673a32
Ryan Pavlik <rpavlik@iastate.edu>
2011-11-01 18:13:55 -0500

    Clean up batch file output.

--- vc9-cmd-prompt-here.cmd
+++ vc9-cmd-prompt-here.cmd
@@ -1,11 +1,11 @@
-rem Set VJ_BASE_DIR to the drive and path of this command script
+@rem Set VJ_BASE_DIR to the drive and path of this command script
 set VJ_BASE_DIR=%~dp0
 
-rem Set VJ_DEPS_DIR to be the same as VJ_BASE_DIR
-set VJ_DEPS_DIR=%VJ_BASE_DIR%
+@rem Set VJ_DEPS_DIR to be the same as VJ_BASE_DIR
+@set VJ_DEPS_DIR=%VJ_BASE_DIR%
 
-rem Extend the PATH to include both the bin and lib directories, so DLLs may be found.
-set PATH=%VJ_BASE_DIR%bin;%VJ_BASE_DIR%lib;%PATH%
+@rem Extend the PATH to include both the bin and lib directories, so DLLs may be found.
+@set PATH=%VJ_BASE_DIR%bin;%VJ_BASE_DIR%lib;%PATH%
 
-rem Run a command prompt that includes the Visual Studio 2008 settings.
-%comspec% /k ""%VS90COMNTOOLS%vsvars32.bat""
+@rem Run a command prompt that includes the Visual Studio 2008 settings.
+@%comspec% /k ""%VS90COMNTOOLS%vsvars32.bat""
